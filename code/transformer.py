7ae79407d59555c5f289d43e383648d34fbd1e6b
Benjamin McLemore <51517288+Chionophile@users.noreply.github.com>
2022-11-29 15:24:09 -0700

    use config file

--- code/transformer.py
+++ code/transformer.py
@@ -4,11 +4,18 @@ import netifaces
 from threading import Thread
 import uuid
 import math
+import json
 
 
-SWITCH_ID = list(uuid.uuid4().bytes)
-SWITCH_ID_LEN = len(SWITCH_ID)
-MTU = 700
+
+
+# get interfaces
+CONFIG = json.loads(sys.stdin.read())
+for k, v in CONFIG.items():
+  print(f"{k}: {v}")
+HOST_IF = CONFIG['host']
+NETWORK_IF = CONFIG['network']
+ID = CONFIG['id']
 
 
 
@@ -27,7 +34,7 @@ def ether_header(dst_iface, ethertype=0x5555):
 
 
 # create the socket server
-def virtualize(plain_if, virtual_if, prot_id):
+def virtualize():
 
   # setup socket server
   in_sock = socket.socket(
@@ -41,8 +48,8 @@ def virtualize(plain_if, virtual_if, prot_id):
     socket.ntohs(3)
   )
   with in_sock, out_sock:
-    in_sock.bind((plain_if,0))
-    out_sock.bind((virtual_if,0))
+    in_sock.bind((HOST_IF,0))
+    out_sock.bind((NETWORK_IF,0))
     while True:
       raw_in_data_bytes, _ = in_sock.recvfrom(65565)
       print('\nIN')
@@ -52,13 +59,10 @@ def virtualize(plain_if, virtual_if, prot_id):
 
       # send the packet
       data_size = len(raw_in_data_list)
-      for i in range(0,data_size,MTU):
-        packet_id = list(uuid.uuid4().bytes)
-        raw_out_data_list = [prot_id] + SWITCH_ID + packet_id + [int(i/MTU), math.ceil(data_size/MTU)]  + raw_in_data_list[i:i+MTU]
-        print(f"{raw_out_data_list=}")
-        payload = ether_header(virtual_if) + bytes(raw_out_data_list)
-        # print(*[hex(b) for b in list(payload)])
-        out_sock.send(payload)
+      raw_out_data_list = raw_in_data_list
+      print(f"{raw_out_data_list=}")
+      payload = ether_header(NETWORK_IF) + bytes(raw_out_data_list)
+      out_sock.send(payload)
       print()
 
 
@@ -92,18 +96,11 @@ def devirtualize(virtual_if, plain_if, id):
 
 
 
-def main(*argv):
-
-  # get sockets
-  plain_if, virtual_if, id = argv
-  id = int(id)
-  print(f"{plain_if=}")
-  print(f"{virtual_if=}")
-  print(f"{id=}")
+def main():
 
   # start virtualizations
   threads = [
-    Thread(target=virtualize, args=[plain_if,virtual_if,id]),
+    Thread(target=virtualize),
     # Thread(target=devirtualize, args=[virtual_if,plain_if,id])
   ]
   for thread in threads:
@@ -114,5 +111,4 @@ def main(*argv):
 
 
 if __name__ == "__main__":
-  print(f"{sys.argv=}")
-  main(*sys.argv[1:])
+  main()

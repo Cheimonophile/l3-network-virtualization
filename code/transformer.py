fde5d7c7733029a2d41a4129dfe8aac089bd0079
Benjamin McLemore <51517288+Chionophile@users.noreply.github.com>
2022-11-29 19:31:17 -0700

    allow ARP

--- code/transformer.py
+++ code/transformer.py
@@ -16,11 +16,13 @@ HOST_MACS = {x['addr'] for x in netifaces.ifaddresses(HOST_IF)[netifaces.AF_LINK
 NETWORK_IF = CONFIG['network']
 NETWORK_MACS = {x['addr'] for x in netifaces.ifaddresses(NETWORK_IF)[netifaces.AF_LINK]}
 ID = CONFIG['id']
+PROTOCOLS = {ether_id: name for name, ether_id in CONFIG['protocols'].items()}
+print(f"{ID=}")
 print(f"{HOST_IF=}")
 print(f"{HOST_MACS=}")
 print(f"{NETWORK_IF=}")
 print(f"{NETWORK_MACS=}")
-print(f"{ID=}")
+print(f"{PROTOCOLS=}")
 
 
 def parse_ether_addr(addr):
@@ -35,12 +37,11 @@ def serialize_ether_addr(addr):
     for b in addr
   )
 
-def ether_header(dst_iface, ethertype=0x5555):
+def ether_header(dst_iface):
   src_ether_addr = netifaces.ifaddresses(dst_iface)[netifaces.AF_LINK][0]['addr']
   src_bytes = parse_ether_addr(src_ether_addr)
   dst_bytes = [0xff]*6
-  ethertype_bytes = list(ethertype.to_bytes(2,'big'))
-  return bytes(dst_bytes + src_bytes + ethertype_bytes)
+  return bytes(dst_bytes + src_bytes)
 
 
 
@@ -52,8 +53,8 @@ def virtualize():
   # setup socket server
   in_sock = socket.socket(
     socket.AF_PACKET,
-    socket.SOCK_DGRAM,
-    socket.ntohs(0x0800) # socket.ntohs(3)
+    socket.SOCK_RAW,
+    socket.ntohs(3) # socket.ntohs(3)
   )
   out_sock = socket.socket(
     socket.AF_PACKET,
@@ -65,14 +66,17 @@ def virtualize():
     out_sock.bind((NETWORK_IF,0))
     while True:
       raw_in_data_bytes, _ = in_sock.recvfrom(65565)
-      print('\nIN')
+      in_protocol = int.from_bytes(raw_in_data_bytes[12:14],'big')
+      if in_protocol not in PROTOCOLS: continue # filter for accepted protocols
+      print()
+      print(f'HOST > NETWORK ({PROTOCOLS[in_protocol]})')
       raw_in_data_list = list(raw_in_data_bytes)
       print(f"{len(raw_in_data_list)=}")
       print(f"{raw_in_data_list=}")
 
       # send the packet
       data_size = len(raw_in_data_list)
-      raw_out_data_list = raw_in_data_list
+      raw_out_data_list = raw_in_data_list[12:]
       print(f"{raw_out_data_list=}")
       payload = ether_header(NETWORK_IF) + bytes(raw_out_data_list)
       out_sock.send(payload)
@@ -100,10 +104,10 @@ def devirtualize():
       raw_in_data_bytes, _ = in_sock.recvfrom(65565)
       src_ether_addr = serialize_ether_addr(raw_in_data_bytes[6:12])
       if src_ether_addr in NETWORK_MACS: continue # skip duplicated traffic
-      print("OUT")
+      print("NETWORK > HOST")
       raw_in_data_list = list(raw_in_data_bytes)
       print(f"{raw_in_data_list=}")
-      raw_out_data_list = raw_in_data_list[14:]
+      raw_out_data_list = raw_in_data_list[12:]
       print(f"{raw_out_data_list=}")
       payload = ether_header(HOST_IF, ethertype=0x0800) + bytes(raw_out_data_list)
       out_sock.send(payload)
